e467bbadcdee08201a84c55784ab61e0b7e2f645
Victor Marcolino <marcolino.victor@gmail.com>
2021-08-20 18:29:49 -0300

    first

---
+++ README.md
@@ -0,0 +1,1 @@
+# Examples Golang with rabbitmq
---
+++ src/main/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"github.com/streadway/amqp"
+	"log"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://rabbitmq:rabbitmq@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+	go receiver(ch, q.Name)
+	something := []int8{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	for _ = range something {
+		go send(ch, q.Name)
+	}
+	forever := make(chan bool)
+	<-forever
+}
+
+func receiver(ch *amqp.Channel, queueName string) {
+	msgs, err := ch.Consume(
+		queueName, // queue
+		"",        // consumer
+		true,      // auto-ack
+		false,     // exclusive
+		false,     // no-local
+		false,     // no-wait
+		nil,       // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+}
+
+func send(ch *amqp.Channel, queueName string){
+	body := "Hello World!"
+	err := ch.Publish(
+		"",     // exchange
+		queueName, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+	log.Printf(" [x] Sent %s", body)
+}
